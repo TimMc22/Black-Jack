67cf81aa779cb32bf83378ea91cb6b86f92f18aa
blawless07 <47438002+blawless07@users.noreply.github.com>
2019-05-23 17:00:20 -0600

    Update Main.java
    Added general methods from Umlet into the backbone of the project

--- BlackJack/src/application/Main.java
+++ BlackJack/src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import models.Card;
+import models.Deck;
+import models.Player;
 
 
 public class Main extends Application {
+	public static Player[] players;
+	public static Deck deck = new Deck();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -22,5 +28,73 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public static Player[] makePlayers() {
+		return players;
+	}
+	
+	public static void gameStart() {
+		
+	}
+	
+	public static void gamePlay() {
+		
+	}
+	
+	public static void dealerPlay() {
+		
+	}
+
+	public static int makeBet() {
+		int bet = 0;
+		return bet;
+	}
+	
+	public static Card[] dealCards() {
+		Card[] hand = new Card[2];
+		return hand;
+	}
+	
+	public static boolean askForHit() {
+		boolean wantHit = false;
+		return wantHit;
+	}
+	
+	public static boolean askForStay() {
+		boolean wantStay = false;
+		return wantStay;
+	}
+	
+	public static boolean splitHand() {
+		boolean wantSplit = false;
+		return wantSplit;
+	}
+	
+	public static boolean doubleDown() {
+		boolean wantDoubleD = false;
+		return wantDoubleD;
+	}
+	
+	public static boolean bust() {
+		boolean isBust = false;
+		return isBust;
+	}
+	
+	public static boolean winCheck() {
+		boolean isWin = false;
+		return isWin;
+	}
+	
+	public static void betOdds() {
+		
+	}
+	
+	public static void saveChips() {
+		
+	}
+	
+	public static void loadChips() {
+		
 	}
 }
