5dcc55d9fed0f13c85969dcf7cb3c066ea49b752
blawless07 <47438002+blawless07@users.noreply.github.com>
2019-05-28 14:51:46 -0600

    Shuffling&NextCardIndex Logic
    Added a shuffle algorythm and added a feature so cards will shuffle when the end of the deck is reached

--- BlackJack/src/models/Deck.java
+++ BlackJack/src/models/Deck.java
@@ -1,29 +1,47 @@
 package models;
 
+import java.util.Random;
+
 public class Deck {
 
 	Card[] deck = new Card[52];
 	int nextCardIndex;
+	public Random rng = new Random();
+
 	public Deck() {
 	}
+
 	public Card[] getDeck() {
 		return deck;
 	}
+
 	public void setDeck(Card[] deck) {
 		this.deck = deck;
 	}
+
 	public int getNextCardIndex() {
 		return nextCardIndex;
 	}
+
 	public void setNextCardIndex(int nextCardIndex) {
 		this.nextCardIndex = nextCardIndex;
+		nextCardIndex++;
+		if (nextCardIndex > 52) {
+			shuffle();
+			nextCardIndex = 0;
+		}
 	}
-	
+
 	public void shuffle() {
-		
+		for (int i = 0; i < deck.length; i++) {
+			int j = rng.nextInt(52) + 1;
+			Card filler = deck[i];
+			deck[i] = deck[j];
+			deck[j] = filler;
+		}
 	}
-	
-	public void reinsertCards() {
-		
+
+	public void reinsertCards(Card lastCard) {
+
 	}
 }
