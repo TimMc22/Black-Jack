2457d803ee10a44b6d939dca5945f8a21a147cfc
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-05-29 10:03:15 -0600

    Update Card.java

--- BlackJack/src/models/Card.java
+++ BlackJack/src/models/Card.java
@@ -6,7 +6,14 @@ public class Card {
 
 	private Face face;
 
+	private int cardValue;
+
 	public Card() {
+		setCardValue(0);
+	}
+
+	public Card(int suit, int val) {
+		setCardValue(val);
 	}
 
 	public Face getFace() {
@@ -16,4 +23,13 @@ public class Card {
 	public void setFace(Face face) {
 		this.face = face;
 	}
+
+	public void setCardValue(int val) {
+		cardValue = val;
+	}
+
+	public int getCardValue() {
+		return cardValue;
+	}
+
 }
